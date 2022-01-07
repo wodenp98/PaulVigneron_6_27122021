8279b0516b0ecb042aa829c52eb77b9a972cd0ab
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-01-07 01:10:36 +0100

    Rate limit

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,8 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 3,
-  });
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 module.exports = { loginLimiter };
